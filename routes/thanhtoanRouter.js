1ac4ee69fc3211a25b2d7d3610b0ac61adae0724
tien hoa <chomaimotnguoi1994@gmail.com>
2021-06-26 06:49:00 +0700

    gg

--- routes/thanhtoanRouter.js
+++ routes/thanhtoanRouter.js
@@ -49,6 +49,47 @@ router.get('/abc', (req, res, next) =>{
 })
 
 router.post('/hoanthanh', (req, res, next)=>{
+    var menuController=require('../controllers/menuController')
+    menuController
+    .menu()
+    .then(data=>{
+        res.locals.menu=data
+        var cart=req.session.cart;
+    res.locals.cart=cart.getCart();
+    if(req.body.diachigiaohang){
+        req.session.cart.diachi=req.body.diachigiaohang
+    }
+    if(req.body.phuongthucgiaohang){
+        req.session.cart.phuongthucgiaohang=req.body.phuongthucgiaohang
+    }
+    if(req.body.hotenkhachhang){
+        req.session.cart.tennguoinhan=req.body.hotenkhachhang
+    }
+    if(req.body.sodienthoai){
+        req.session.cart.sodienthoai=req.body.sodienthoai
+    }
+    if(req.body.emailnguoinhan){
+        req.session.cart.email=req.body.emailnguoinhan
+    }
+    if(req.body.ghichu){
+        req.session.cart.ghichu=req.body.ghichu
+    }
+        
+        let nhanvienController = require('../controllers/nhanvienController')
+    
+        return nhanvienController.layhetnhanvien()
+    })
+    .then((data)=>{
+        res.locals.nhanviens=data;
+        
+            res.render('hoanthanh'); 
+        
+        
+        
+    })
+    .catch(error=>next(error));
+})
+router.get('/hoanthanh', (req, res, next) =>{
     var cart=req.session.cart;
     res.locals.cart=cart.getCart();
     if(req.body.diachigiaohang){
@@ -69,35 +110,7 @@ router.post('/hoanthanh', (req, res, next)=>{
     if(req.body.ghichu){
         req.session.cart.ghichu=req.body.ghichu
     }
-    
-    
-    
-    var menuController=require('../controllers/menuController')
-    menuController
-    .menu()
-    .then(data=>{
-        res.locals.menu=data
-        
-        let nhanvienController = require('../controllers/nhanvienController')
-    
-        return nhanvienController.layhetnhanvien()
-    })
-    .then((data)=>{
-        res.locals.nhanviens=data;
-        
-            res.render('hoanthanh'); 
-        
-        
-        
-    })
-    .catch(error=>next(error));
-})
-router.get('/hoanthanh', (req, res, next) =>{
-    var cart=req.session.cart;
-    res.locals.cart=cart.getCart();
-    
-    
-    
+
     var menuController=require('../controllers/menuController')
     menuController
     .menu()
